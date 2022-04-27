6a677f5d31a0b9db8f170e4383f36ebbcd3cd178
Farrux45 <farxoshbakov@gmail.com>
2022-04-27 22:53:33 +0500

    second upload

--- theme.dart
+++ theme.dart
@@ -316,10 +316,10 @@
       disabledInactiveTickMarkColor: null,
       thumbColor: null,
       disabledThumbColor: null,
-      thumbShape: null(),
+      thumbShape: null,
       overlayColor: null,
       valueIndicatorColor: null,
-      valueIndicatorShape: null(),
+      valueIndicatorShape: null,
       showValueIndicator: null,
       valueIndicatorTextStyle: const TextStyle(
       color: Color( 0xdd000000 ),
